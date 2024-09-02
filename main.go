d8584cc3c204bd20f9726a79d39b4c14b785a24e
unknown <ajiqdin646@gmail.com>
2024-09-03 01:03:50 +0800

    added add function

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+
+import "time"
+
+
+type item struct{
+	Task string
+	Completed boolean
+	CreatedAt time.Time
+	CompletedAt time.Time
+
+}
+
+type ToDos []item
+
+
+func (t *ToDos) add(task string){
+	todo := item{
+		Task :task,
+		Done : false ,
+		CreatedAt : time.Now(),
+		CompletedAt : time.Time{},
+
+	}
+
+	*t = append(*t , todo)
+}
+
+
+func (t *ToDos) complete(index int) error{
+	ls := *t
+
+	if index <= 0 || index > len(ls){
+		return errors.New("invalid index")
+	}
+
+	ls[index-1].CompletedAt = time.Now()
+	ls[index-1].Done = true
+
+	return nil
+}
