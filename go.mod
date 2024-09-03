df837276b61aed41dcd4598d16c944f554e13128
unknown <ajiqdin646@gmail.com>
2024-09-03 17:12:59 +0800

    added main.go for initialisation

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module main
+module todo
 
 go 1.22.4
--- main.go
+++ main.go
@@ -1,53 +1,13 @@
 package main
 
 
-import "time"
+import "fmt"
 
 
-type item struct{
-	Task string
-	Completed boolean
-	CreatedAt time.Time
-	CompletedAt time.Time
-
-}
-
-type ToDos []item
-
-
-func (t *ToDos) add(task string){
-	todo := item{
-		Task :task,
-		Done : false ,
-		CreatedAt : time.Now(),
-		CompletedAt : time.Time{},
-
-	}
-
-	*t = append(*t , todo)
+func main(){
+	fmt.Println("hello world")
 }
 
 
-func (t *ToDos) complete(index int) error{
-	ls := *t
 
-	if index <= 0 || index > len(ls){
-		return errors.New("invalid index")
-	}
 
-	ls[index-1].CompletedAt = time.Now()
-	ls[index-1].Done = true
-
-	return nil
-}
-
-func ( t* ToDos) delete(index int) error {
-	ls := *t
-		if index <= 0 || index > len(ls){
-		return errors.New("invalid index")
-	}
-
-	*t = append(ls[::index-1] , ls[index:]...)
-
-    return nil
-}
